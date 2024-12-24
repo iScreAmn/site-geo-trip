9f12daa597086d974567afde110a45c859702454
Dimitri. J <jmukhadze.dimitri@gmail.com>
2024-12-25 01:21:33 +0400

    Added toggle functionality for the mobile navigation button

--- scripts/main.js
+++ scripts/main.js
@@ -108,8 +108,10 @@ document.addEventListener("DOMContentLoaded", updateActiveNav);
 // Burger-Menu
 
 const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
 const menuIcon = document.querySelector(".menu-icon");
 
 navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
   menuIcon.classList.toggle("menu-icon--active");
 };
